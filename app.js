fa769afcb98b8f6f67606248ece1285ac80f8bfb
cristinel.pirvulescu <pirvulescu.cristi@gmail.com>
2016-03-20 00:33:54 +0200

    Add PUT route function

--- app.js
+++ app.js
@@ -40,7 +40,17 @@ app.get('/api/books', getBooks);
 
 app.post('/api/books', addBook);
 
-app.put('/api/books/:id', function(req, res) {});
+app.put('/api/books/:id', function(req, res) {
+  const reqBody = req.body;
+
+  Book.findOneAndUpdate({ _id: '56edcb41aef1a07f610f2256' }, reqBody, (err) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
 
 app.delete('/api/books/:id', function(req, res) {});
 
