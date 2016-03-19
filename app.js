6ae3d7df027681b96a641ea1a394690a4550d9ba
cristinel.pirvulescu <pirvulescu.cristi@gmail.com>
2016-03-19 23:34:51 +0200

    Add get route function

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
 const express = require('express');
-const libApp = express();
+const app = express();
 const Book = require('app/models/book.js');
 
-/**
- * Retrieve all books from database
- * @param  {[type]} res [description]
- * @return {[type]}     [description]
- */
-function getBooks(res) {
-  Book.find(function(err, books) {
-    if (err) { res.send(err); }
+const getBooks = (req, res) => {
+  Book.find((err, books) => {
+    if (err) {
+      return res.send(err);
+    }
 
-    res.json(books);
+    return res.json(books);
   });
-}
+};
 
-express.get('/api/books', function(req, res) {});
+app.get('/api/books', getBooks);
 
-express.post('/api/books', function(req, res) {});
+app.post('/api/books', function(req, res) {});
 
-express.put('/api/books/:id', function(req, res) {});
+app.put('/api/books/:id', function(req, res) {});
 
-express.delete('/api/books/:id', function(req, res) {});
+app.delete('/api/books/:id', function(req, res) {});
 
-express.get('*', function(req, res) {});
+app.get('*', function(req, res) {});
 
-express.listen(3000, function() {
+app.listen(3000, function() {
   console.log('listening on port 3000');
-})
+});
