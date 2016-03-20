d97a8985013fd7e3aa1e2520c702fa2534dc28d1
cristinel.pirvulescu <pirvulescu.cristi@gmail.com>
2016-03-21 01:29:28 +0200

    Update Routes module - Add middleware for checking authetication-token on all routes except /signup, /auth

--- routes/routes.js
+++ routes/routes.js
@@ -124,10 +124,41 @@ const authenticateUser = (req, res) => {
   });
 };
 
-router.post('/signup', createUser);
+const verifyAuthToken = (req, res, next) => {
+  // check header or url parameters or post parameters for token
+  const token = req.body.token || req.query.token || req.headers['x-access-token'];
 
+  // decode token
+  if (token) {
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('secretToken'), (err, decoded) => {
+      if (err) {
+        return res.json({
+          success: false,
+          message: 'Failed to authenticate token.',
+        });
+      }
+
+      // if everything is good, save to request for use in other routes
+      req.decoded = decoded;
+      next();
+    });
+  } else {
+    // if there is no token
+    // return an error
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided.',
+    });
+  }
+};
+
+router.post('/signup', createUser);
 // authenticate route
 router.post('/auth', authenticateUser);
+
+// route middleware to verify a token
+router.use(verifyAuthToken);
 
 router.get('/books', getBooks);
 
