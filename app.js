6eef1fb81e4fc0e6cb11b7ef2d30080bbd30233f
cristinel.pirvulescu <pirvulescu.cristi@gmail.com>
2016-03-20 22:23:00 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const Book = require('./models/book');
+const config = require('./config');
 const routes = require('./routes/routes');
 
-//app.use(express.static(__dirname + '/'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set('secretToken', config.secret);
 /* Routes setup */
 app.use('/api', routes);
 
