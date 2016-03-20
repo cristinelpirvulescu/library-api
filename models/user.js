6b8a6b5d5b34157d7e4ca61e895dfca0061b56f3
cristinel.pirvulescu <pirvulescu.cristi@gmail.com>
2016-03-20 22:20:01 +0200

    Update User model - add encryption functionality

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
 
 const UserSchema = new Schema({
   name: {
@@ -14,5 +16,47 @@ const UserSchema = new Schema({
     required: true,
   },
 });
+
+UserSchema.pre('save', function(next) {
+  const user = this;
+  console.log('pre-save');
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) {
+    console.log('new-pass');
+    next();
+  }
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) {
+      console.log('err-genSalt');
+      // next(err);
+    }
+
+    // hash the password with the new salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) {
+        console.log('err-hash-pass');
+        // next();
+      }
+
+      // override the clear_text password with the hashed one
+      user.password = hash;
+      console.log(user.password);
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) {
+      console.log('cb-err');
+      callback(err);
+    }
+
+    console.log('compare');
+    callback(null, isMatch);
+  });
+};
 
 module.exports = mongoose.model('User', UserSchema);
