d7369efde386c2260095558954311a86c5675f3e
cristinel.pirvulescu <pirvulescu.cristi@gmail.com>
2016-03-26 11:15:34 +0200

    Update config.js file - Remove local database reference, add link to Modulus.io database

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
   secret: 'supersecretkey',
-  database: 'mongodb://localhost/library_db',
+  //database: 'mongodb://localhost/library_db',
+  database: 'mongodb://test:test@jello.modulusmongo.net:27017/guzo7zyR',
 };
