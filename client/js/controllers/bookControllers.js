4aced7ad7baff37219cf3bf877037c436a44d1af
cristinel.pirvulescu <pirvulescu.cristi@gmail.com>
2016-03-29 02:13:04 +0300

    Update bookControllers - Inject services (books.js) and use $httpParamSeriliazer to send correct data

--- client/js/controllers/bookControllers.js
+++ client/js/controllers/bookControllers.js
@@ -1,15 +1,38 @@
-const bookControllers = angular.module('bookControllers', []);
+const bookControllers = angular.module('bookControllers', ['bookService']);
 
-bookControllers.controller('BookListController', ['$scope', '$http',
-  ($scope, $http) => {
-    $http.get('http://localhost:3000/api/books')
+bookControllers.controller('BookListController', ['$scope', '$http', '$httpParamSerializer', 'Books',
+  ($scope, $http, $httpParamSerializer, Books) => {
+    $scope.form = {};
+    $scope.loading = {};
+
+    Books.get()
       .success((data) => {
         $scope.books = data;
-        console.log(data);
+        $scope.loading = false;
       })
       .error((err) => {
         console.log(err);
       });
+
+    $scope.createBook = () => {
+      if ($scope.form != undefined) {
+        $scope.loading = true;
+
+        console.log($httpParamSerializer($scope.form));
+        Books.create($httpParamSerializer($scope.form))
+          .success((data) => {
+            $scope.loading = false;
+            $scope.form = {};
+            $scope.books = data;
+          })
+          .error((err) => {
+            console.log('Error: ' + err);
+          });
+      } else {
+        console.log($scope);
+      }
+
+    };
 
     $scope.orderProp = 'title';
   }]);
