ffbc56ff68d24e7d5d4e58d493bc1bbb834fb660
cristinel.pirvulescu <pirvulescu.cristi@gmail.com>
2016-03-29 02:01:58 +0300

    Update app.js - Add cross-domain handler function

--- api/app.js
+++ api/app.js
@@ -9,10 +9,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('secretToken', config.secret);
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
+const allowCrossDomain = function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+
+  // Request methods you wish to allow
+ res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,x-access-token');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.sendStatus(200);
+  }
+  else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
 
 /* Routes setup */
 app.use('/api', routes);
